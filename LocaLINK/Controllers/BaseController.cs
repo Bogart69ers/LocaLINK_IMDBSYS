51e9e88abbf4ea5a58a41696f6abdd29beb9ed20
Angldrie101 <89821301+Angldrie101@users.noreply.github.com>
2024-04-18 13:16:14 +0800

    Add project files.

---
+++ LocaLINK/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using LocaLINK.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LocaLINK.Controllers
+{
+    public class BaseController : Controller
+    {
+        public LOCALinkEntities _db;
+        public BaseRepository<User_Account> _userRepo;
+        public BaseRepository<Booking> _booking;
+        public BaseController()
+        {
+            _db = new LOCALinkEntities();
+            _userRepo = new BaseRepository<User_Account>();
+            _booking = new BaseRepository<Booking>();
+
+        }
+    }
+}
---
+++ LocaLINK/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace LocaLINK.Controllers
+{
+    [Authorize(Roles = "User, Worker, Admin")]
+    public class HomeController : BaseController
+    {
+        [AllowAnonymous]
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_userRepo.GetAll());
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(User_Account u)
+        {
+            var user = _userRepo._table.Where(m => m.user_name == u.user_name).FirstOrDefault();
+
+            if (user != null)
+            {
+                FormsAuthentication.SetAuthCookie(u.user_name, false);
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "User does not Exist or Incorrect Password");
+
+            return View(u);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
